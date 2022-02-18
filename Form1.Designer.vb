8bdc25fae20ec162ab6f432f74bc818e693f07a9
miniyu157 <miniyu157@163.com>
2022-02-18 13:56:06 +0800

    添加项目文件。

---
+++ Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("微软雅黑", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(42, 104)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(708, 304)
+        Me.TextBox2.TabIndex = 7
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("微软雅黑", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(379, 34)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(371, 29)
+        Me.TextBox1.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("微软雅黑", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label1.Location = New System.Drawing.Point(38, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(335, 21)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "待分解数字：(上限9223372036854775807)："
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "数字分解程序"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+        If TextBox1.Text <> "" And Microsoft.VisualBasic.Left(TextBox1.Text, 1) <> "-" And Microsoft.VisualBasic.Right(TextBox1.Text, 1) <> "-" Then
+            Try
+                TextBox2.Text = ""
+                Dim n1 As Long = TextBox1.Text '不能超过9223372036854775807
+                Dim n16 = Convert.ToString(n1, 16)
+
+                For i = 1 To Len(n16)
+
+                    Dim f5da2 = Microsoft.VisualBasic.Right(Microsoft.VisualBasic.Left(n16, i), 1) '提取每一个字符
+
+                    If f5da2 = "a" Then
+                        f5da2 = 10
+                    ElseIf f5da2 = "b" Then
+                        f5da2 = 11
+                    ElseIf f5da2 = "c" Then
+                        f5da2 = 12
+                    ElseIf f5da2 = "d" Then
+                        f5da2 = 13
+                    ElseIf f5da2 = "e" Then
+                        f5da2 = 14
+                    ElseIf f5da2 = "f" Then
+                        f5da2 = 15
+                    End If
+
+                    TextBox2.Text = TextBox2.Text & f5da2 & " * 16 ^ " & (Len(n16) - i) & " + "
+
+                Next
+
+                TextBox2.Text = Microsoft.VisualBasic.Left(TextBox2.Text, Len(TextBox2.Text) - 3)
+
+                TextBox2.Text = n1 & " = " & TextBox2.Text
+
+            Catch ex As Exception
+                TextBox2.Text = ""
+            End Try
+        Else
+            TextBox2.Text = ""
+        End If
+
+    End Sub
+
+End Class
